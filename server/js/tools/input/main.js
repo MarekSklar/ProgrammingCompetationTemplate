f12e5c627845f798bbdc1aa7d55176ed8c9f2b5f
Marek Sklář <99105387+MarekSklar@users.noreply.github.com>
2024-04-18 23:28:42 +0200

    Delete server/js directory

--- server/js/tools/input/main.js
+++
@@ -1,7 +0,0 @@
-import readline from 'node:readline/promises';
-const rl = readline.createInterface(process.stdin, process.stdout);
-export default async function input(question) {
-    const answer = rl.question(question);
-    return answer;
-}
-//# sourceMappingURL=main.js.map
